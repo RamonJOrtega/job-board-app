ae2c54806c1856dbee97b313a4f570777f8c4d21
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-06-16 10:41:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
+
